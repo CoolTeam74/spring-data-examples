4c1f557dbd8abb7bbec70dc674534ecaaee48c79
A.Polyakov <a.polyakov@unlimint.com>
2022-12-18 11:57:09 +0300

    fix SearchSpecification

--- ui-custom-filters-starter/src/main/java/ru/apolyakov/ui_custom_filters/service/SearchSpecification.java
+++ ui-custom-filters-starter/src/main/java/ru/apolyakov/ui_custom_filters/service/SearchSpecification.java
@@ -1,5 +1,6 @@
 package ru.apolyakov.ui_custom_filters.service;
 
+import com.google.common.collect.Lists;
 import lombok.RequiredArgsConstructor;
 import ru.apolyakov.ui_custom_filters.dto.Operation;
 import ru.apolyakov.ui_custom_filters.dto.SearchCriteria;
@@ -10,6 +11,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class SearchSpecification<T> implements Specification<T> {
@@ -21,17 +23,49 @@ public class SearchSpecification<T> implements Specification<T> {
         if (searchCriteria.getTable() != null) {
             join = root.join(searchCriteria.getTable());
         }
-
-        if (Operation.GR.getAlias().equalsIgnoreCase(searchCriteria.getOperator())) {
-            return criteriaBuilder.greaterThan(root.<String>get(searchCriteria.getKey()), searchCriteria.getValue().toString());
-        } else if (Operation.EQ.getAlias().equalsIgnoreCase(searchCriteria.getOperator())) {
+        if (Operation.GR.name()
+                .equalsIgnoreCase(searchCriteria.getOperator())) {
+            return criteriaBuilder.greaterThanOrEqualTo(
+                    root.<String> get(searchCriteria.getKey()),
+                    searchCriteria.getValue().toString());
+        }
+        else if (Operation.LO.name()
+                .equalsIgnoreCase(searchCriteria.getOperator())) {
+            return criteriaBuilder.lessThanOrEqualTo(
+                    root.<String> get(searchCriteria.getKey()),
+                    searchCriteria.getValue().toString());
+        }
+        else if (Operation.EQ.name()
+                .equalsIgnoreCase(searchCriteria.getOperator())) {
             if (searchCriteria.getTable() != null) {
-                return criteriaBuilder.equal(join.get(searchCriteria.getKey()), searchCriteria.getValue());
+                if (join.get(searchCriteria.getKey()).getJavaType() == String.class) {
+                    return criteriaBuilder.like(
+                            criteriaBuilder.lower(join.get(searchCriteria.getKey())),
+                            "%" + searchCriteria.getValue().toString().toLowerCase() + "%");
+                } else {
+                    return criteriaBuilder.equal(join.get(searchCriteria.getKey()),
+                            searchCriteria.getValue());
+                }
             } else {
-                criteriaBuilder.equal(root.get(searchCriteria.getKey()), searchCriteria.getValue());
+                if (root.get(searchCriteria.getKey()).getJavaType() == String.class) {
+                    return criteriaBuilder.like(
+                            criteriaBuilder.lower(root.get(searchCriteria.getKey())),
+                            "%" + searchCriteria.getValue().toString().toLowerCase() + "%");
+                } else {
+                    return criteriaBuilder.equal(root.get(searchCriteria.getKey()),
+                            searchCriteria.getValue());
+                }
+            }
+        } else if (Operation.IN.name()
+                .equalsIgnoreCase(searchCriteria.getOperator())) {
+            List<String> values =
+                    Lists.newArrayList((List<String>)searchCriteria.getValue());
+            if (searchCriteria.getTable() != null) {
+                return join.get(searchCriteria.getKey()).in(values);
+            } else {
+                return root.get(searchCriteria.getKey()).in(values);
             }
         }
-
         return null;
     }
 }
